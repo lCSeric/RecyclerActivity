c7cfc685fd1ba69fad4e379d0a6f5f279dc67f29
Eric Lin <tur99299@temple.edu>
2024-02-14 16:30:58 -0500

    In the first step, create a recyclerView to reference it.
    In the second step, we create a linear layout ,and define a number with Integer array, with size 100.
    and use for loop to create the number from 1 to 100
    
    In the step 4, create val adapter to display the number

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,37 @@
 package edu.temple.simplerecyclerview
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
+/* Step 3a: Provide Adapter Parent */
+class NumberDisplayAdapter(number: IntArray) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+    private val number = number
+    class NumberViewHolder(_textView: TextView) : RecyclerView.ViewHolder (_textView) {
+        val textV = _textView
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
+    }
     //Step 3b: Complete function definitions for adapter
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(
+            TextView(
+                parent.context
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return number.size
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.textV.text= number[position].toString()
+    }
+
 
 }
